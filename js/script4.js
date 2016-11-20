95b61175e948c3771d0767c99c5569deddaf811a
odevoo <elandry@odevoo.com>
2016-11-20 20:12:37 +0100

    commit

---
+++ js/script4.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+
+    $(window).scroll( function(){
+
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      // Check the location of each element hidden */
+      $('.experience .content .hidden').each( function(i){
+
+          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+
+          /* If the object is completely visible in the window, fadeIn it */
+          if( bottom_of_window > bottom_of_object ){
+
+            $(this).animate({
+              'opacity':'1',
+              'margin-left': '0'
+            },600);
+          }
+      });
+
+});
+
+});
