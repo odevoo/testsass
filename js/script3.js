2753979e266bd720aac96988666cdb77822bf7f6
odevoo <elandry@odevoo.com>
2016-11-22 11:17:39 +0100

    commit

--- js/script3.js
+++ js/script3.js
@@ -1,72 +1,27 @@
-$(document).ready(function() {
-    $('.trigger').click(function() {
-      var $this = $(this),
-      notThis = $this.hasClass('open'),
-      thisNav = $this.attr("rel");
-      /*$('#title1').delay(350).queue(function(){
-        $(this).addClass('invisible').clearQueue();
-      });*/
-      $('#resume').toggleClass('invisible');
-      $('#title1').toggleClass('invisible');
-      $('#title2').toggleClass('invisible');
-      //close if you click another menu trigger
-      if (!notThis) {
-        
-        
-         $('.triggered-area, .trigger').removeClass('open');
-         if ( $( '.trigger' ).hasClass( "block-scroll" ) ){
-            console.log('remove');
-            $('html').removeClass('disable-scroll');
-            $('#overlay-mobile').removeClass('visible');
-         }
-      }
 
-      //open the nav
-      
-      $this.toggleClass('open');
-      $("#"+thisNav).toggleClass('open');
-      if ( $( this ).hasClass( "block-scroll" ) ){
-        
-         $('html').toggleClass('disable-scroll');
-         $('#overlay-mobile').toggleClass('visible');
-      }
-    });
+$(document).ready(function(){
 
-    /*********************/
-
-    $('.linkmenu').click(function() {
-      console.log('ok');
-      var $this = $(this),
-      notThis = $this.hasClass('open'),
-      thisNav = $this.attr("rel");
-      /*$('#title1').delay(350).queue(function(){
-        $(this).addClass('invisible').clearQueue();
-      });*/
-
-      $('#title1').toggleClass('invisible');
-      $('#title2').toggleClass('invisible');
-      //close if you click another menu trigger
-      if (!notThis) {
-        
-        
-         $('.triggered-area, .trigger').removeClass('open');
-         if ( $( '.trigger' ).hasClass( "block-scroll" ) ){
-            console.log('remove');
-            $('html').removeClass('disable-scroll');
-            $('#overlay-mobile').removeClass('visible');
-         }
-      }
-
-      //open the nav
-      
-      $this.toggleClass('open');
-      $("#"+thisNav).toggleClass('open');
-      if ( $( this ).hasClass( "block-scroll" ) ){
-        
-         $('html').toggleClass('disable-scroll');
-         $('#overlay-mobile').toggleClass('visible');
-      }
-    });
-
+ $('.trigger').on('click', function() {
+   var thisNav = $(this).attr("rel");
+   $('.nav-content').toggleClass('invisible');
+   $('#resume').toggleClass('invisible');
+   $('#title1').toggleClass('invisible');
+   $('#title2').toggleClass('invisible');
+   $(this).toggleClass('open');
+   $("#"+thisNav).toggleClass('open');
 
 });
+
+ $('.linkmenu').on('click', function(){
+   var thisNav = $('.trigger').attr("rel");
+   $('.nav-content').toggleClass('invisible');
+   $('#resume').toggleClass('invisible');
+   $('#title1').toggleClass('invisible');
+   $('#title2').toggleClass('invisible');
+   $('.trigger').toggleClass('open');
+   $("#"+thisNav).toggleClass('open');
+ }); 
+
+});
+
+
